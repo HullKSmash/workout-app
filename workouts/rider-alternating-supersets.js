0ba914a686be8d897240084b8a3dda210f4b4901
Katie Hull <katie.ann.hull@gmail.com>
2026-06-16 18:07:47 -0700

    updating description and difficulty of new workout

--- workouts/rider-alternating-supersets.js
+++ workouts/rider-alternating-supersets.js
@@ -3,8 +3,8 @@ export default {
   "audiences": [
     "equestrian"
   ],
-  "difficulty": "harder",
-  "description": "Upper/lower alternating supersets for maximum training efficiency",
+  "difficulty": "easier",
+  "description": "Alternating unilateral exercises in supersets.  A fun and lighter take on symmetry work!",
   "phases": [
     {
       "name": "Warm Up",
