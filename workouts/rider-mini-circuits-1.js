28b0040ecaaa446f188628b0393128ac76e982a5
Katie Hull <katie.ann.hull@gmail.com>
2026-08-25 17:48:10 -0700

    Adding back alternating designation to standing abduction

--- workouts/rider-mini-circuits-1.js
+++ workouts/rider-mini-circuits-1.js
@@ -24,6 +24,7 @@ export default {
             },
             {
               "name": "Standing Abduction",
+              "side": "Alternating",
               "repCount": "5-10 per side",
               "tips": "Keep your balance as you rotate your knee out, using your glutes to open your hip."
             }
